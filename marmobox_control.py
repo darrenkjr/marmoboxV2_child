c219d1b2c860214ef3b43d0b463acddb36c21679
Darren <darren.rajit@gmail.com>
2019-05-02 12:45:52 +1000

    changes to file directory structure

--- marmobox_control.py
+++ marmobox_control.py
@@ -1,10 +1,11 @@
 #import classes from arduino control and from tasks - runs declared tasks, pre-import required modules + packages
 
 import arduino_controlclass as control
-import tasks.draw_framesRL as test
+from marmobox import tasklist
 from psychopy import visual, core, logging, event
 
 mywin = visual.Window([1280, 720], monitor="testMonitor", units="pix", pos=(0, 0))
-taskname = 'draw_framesRL'
+print('test')
 
-test
+task = importlib.import_module()
+task1.run(mywin)
