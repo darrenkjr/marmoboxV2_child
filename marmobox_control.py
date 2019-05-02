e7568c7f4770baf24a6cf3e617d6acbb73e756fb
Darren <darren.rajit@gmail.com>
2019-05-02 13:47:47 +1000

    we did it team

--- marmobox_control.py
+++ marmobox_control.py
@@ -1,11 +1,17 @@
 #import classes from arduino control and from tasks - runs declared tasks, pre-import required modules + packages
 
 import arduino_controlclass as control
-from marmobox import tasklist
 from psychopy import visual, core, logging, event
+import importlib
 
-mywin = visual.Window([1280, 720], monitor="testMonitor", units="pix", pos=(0, 0))
-print('test')
+tasklist = ['tasks.drawframesRL']
 
-task = importlib.import_module()
-task1.run(mywin)
+for x in tasklist:
+    print(x)
+    task1 = importlib.import_module(x)
+    # import tasks.touchtraining1 as task2
+
+    mywin = visual.Window([1280, 720], monitor="testMonitor", units="pix", pos=(0, 0))
+    print('test')
+
+    task1.run(mywin)
