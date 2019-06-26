924f16899a26abf34ec9bcc44c64563b04718759
Kieren Pinto <kieren.pinto@hotmail.com>
2019-06-26 17:29:35 +1000

    Readme, Requirements, Arduino Pump Added

--- arduinocontrol.py
+++ arduinocontrol.py
@@ -1,1 +1,62 @@
 #classes associated with behaviour reinforcement and reward dispensing systems, and correct + incorrect classes
+import serial #Requires pyserial - pip install pyserial
+import time
+
+class arduino_control:
+    def __init__(self):
+        print("Setup Arduino")
+        self.connect()
+        self.debug = False
+        return
+    
+    def connect(self):
+        '''
+        Initiate Uart Connection
+        '''
+        self.pump_arduino = serial.Serial('/dev/ttyACM0',9600,timeout=1)
+        #note that aruduino restarts on serial connection
+        time.sleep(0.5) #ensure that arduino has started before proceeding
+        return
+
+    def disconnect(self):
+        '''
+        Disconnect from uart
+        '''
+        self.pump_arduino.close()
+        return
+    
+    def set_debug(self,val):
+        '''
+        Set debug val as true or false
+        Debug enables print statments in the arduino_control class
+        '''
+        self.debug = val
+        return
+
+    def pump(self,volume):
+        ''' Dispenses a set (volume) from the pump'''
+        # Run pump
+        time = self.volume_to_time(volume)
+        self.pump_arduino.write(bytes('M{millis}\r'.format(millis=time),'utf-8'))
+        if self.debug:
+            print("Dispensed {volume} microlitres in {time} ms \n".format(volume=volume,time=time))
+        return 
+
+    def volume_to_time(self,volume):
+        '''volume in uL (micro litres) return: time in milliseconds that the pump is on'''
+        # Function that maps volume to time
+        time = volume * 1.75 # uL/ms
+        return time
+
+
+class arduino_calibration:
+    def __init__(self):
+        return
+
+    def start_timer(self):
+        return
+
+    def read_timer(self):
+        timer_value = 0
+        #read delta time back from arduino on UART
+        return timer_value
---
+++ readme.md
@@ -0,0 +1,13 @@
+# Introduction
+This repository contains the code for the client (child) of the Marmobox version 2.
+
+# Requirements
+
+You must have python 3 or greater. Then you must install the dependancies from requirements.txt
+
+# Installation Instructions
+
+Run
+<code>
+pip3 install -r requirements.txt
+</code>
