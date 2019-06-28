1f3645fd6fbfe3daef223968b545bd46d4dd6f06
Darren <darren.rajit@gmail.com>
2019-06-28 14:16:26 +1000

    add unique coord

--- tasks/touch_latencytest.py
+++ tasks/touch_latencytest.py
@@ -1,5 +1,6 @@
 from psychopy import visual, event
 import time, datetime
+import numpy as np
 
 
 mywin = visual.Window([1280, 720], monitor="testMonitor", units="pix", pos=(0, 0))
@@ -18,18 +19,24 @@ while not mouse.getPressed()[0]:
 
 if mouse.isPressedIn(grating) == True:
 	start_time = datetime.datetime.now()
+	x_coord = []
+	y_coord = []
 	while mouse.isPressedIn(grating):
 
 		counter +=1
 		print(counter)
-		print(mouse.getPos()[0],mouse.getPos()[1])
+		x_coord.append(mouse.getPos()[0])
+		y_coord.append(mouse.getPos()[1])
 
 if counter >1:
 	end_time = datetime.datetime.now()
 	time_delta = (end_time - start_time).total_seconds()
 	print((time_delta))
+	coord = [x_coord,y_coord]
 
 print(counter,time_delta)
+uniques, counts = np.unique(coord,return_counts = True)
+print(uniques, counts)
 sample_rate = counter / time_delta
 
 print('Sample rate (per second): ', sample_rate)
