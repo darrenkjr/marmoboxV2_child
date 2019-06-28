657874a2bfa8c38ba1c616ac9809a0869b1a8864
Darren <darren.rajit@gmail.com>
2019-06-28 14:31:45 +1000

    changed touch latency test

--- tasks/touch_latencytest.py
+++ tasks/touch_latencytest.py
@@ -3,7 +3,7 @@ import time, datetime
 import numpy as np
 
 
-mywin = visual.Window([800, 480], monitor="testMonitor", units="pix", pos=(0, 0))
+mywin = visual.Window([1280, 720], monitor="testMonitor", units="pix", pos=(0, 0))
 grating = visual.GratingStim(win=mywin, size=200, pos=[0,0], sf=0, color = [-1,-1,1], colorSpace='rgb')
 mouse = event.Mouse(win=mywin)
 
