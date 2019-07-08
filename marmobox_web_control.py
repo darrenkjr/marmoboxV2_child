eb91011958c8e55006e9183877c2d654de69ac9d
Kieren Pinto <kieren.pinto@hotmail.com>
2019-07-08 10:07:35 +1000

    New marmobox web control fixed additions to gitignore

---
+++ marmobox_web_control.py
@@ -0,0 +1,74 @@
+#import classes from arduino control and from tasks - runs declared tasks, pre-import required modules + packages
+import json
+import time
+from psychopy import visual
+import importlib
+from tasks.helper.json_handler import json_handler
+import datetime
+import multiprocessing as mp
+import os
+from http.server import BaseHTTPRequestHandler, HTTPServer
+from flask import render_template, Flask, jsonify, request
+
+def loadtaskmodule(tasklist):
+    '''
+        loading required tasks, and assigns each task a number
+    '''
+    taskmodule = []
+    for x in tasklist:
+        print(x)
+        task = importlib.import_module(x)
+        taskmodule.append(task)
+        print('test')
+        print(taskmodule)
+
+    return taskmodule
+
+def execute_command(q_in,q_out):
+    '''
+        Main function for marmobox_control.py
+    '''
+    # mywin = visual.Window([1280, 720], monitor="testMonitor", units="pix", pos=(0, 0))
+    while True: 
+        json_command = q_in.get()
+        # load in required tasklist and instructions from json and other paramters (animal_ID etc)
+        jsonHandler = json_handler()
+        taskname, animalID = jsonHandler.read_input(json_command)
+        session = 0
+        taskmodule = importlib.import_module(taskname)
+
+        print('running ', taskname)
+        results = taskmodule.run(taskname,animalID,session) #args = taskname,limitTrial,mywin,animal_ID,session
+        timestamp = datetime.datetime.now()
+        json_output = jsonHandler.create_json_output(results,animalID,str(timestamp))
+        json_output = 'test_complete'
+        q_out.put(json_command)
+
+# Create a URL route in our application for "/"
+if __name__ == "__main__":
+    mp.set_start_method('spawn')
+    q_in = mp.Queue()
+    q_out = mp.Queue()
+app = Flask(__name__)
+# Create a URL route in our application for "/"
+@app.route('/', methods=["GET","POST"])
+def main():
+    '''
+        Calls a trial to be run as per the JSON instruction
+    '''
+    json_string = request.data
+    q_in.put(json_string)
+    out = q_out.get()
+    print("The output is: {0}".format(out))
+    return jsonify({out})
+
+if __name__ == "__main__":
+    json_command = '{"taskname":"tasks.touch-training0","animal_ID":"test"}'
+    # q_in.put(json_command)
+    p = mp.Process(target=execute_command, args=(q_in,q_out))
+    p.start()
+    # server_address = ('', 8000)
+    # httpd = HTTPServer(server_address,BaseHTTPRequestHandler)
+    # httpd.serve_forever()
+    app.run(debug=True)
+    p.join()
--- serve.py
+++ serve.py
@@ -2,12 +2,12 @@ from flask import render_template, Flask, jsonify, request
 import json
 import time
 import marmobox_control
+# from multiprocessing import Process
 import multiprocessing as mp
 import os
+def command(json_string,return_val):
+    return_val = marmobox_control.execute_command(json_string)
 
-mp.set_start_method('spawn')
-q = mp.Queue()
-p = mp.process
 app = Flask(__name__)
 # Create a URL route in our application for "/"
 @app.route('/', methods=["GET","POST"])
@@ -16,10 +16,15 @@ def main():
         Calls a trial to be run as per the JSON instruction
     '''
     json_string = request.data
-    marmobox_control.execute_command(json_string)
-    return jsonify(pythonobj)
+    manager = mp.Manager()
+    return_val = manager.list()
+    p = mp.Process(target=command, args=(json_string,return_val))
+    p.start()
+    p.join()
+    return jsonify({})
 
 # If we're running in stand alone mode, run the application
 if __name__ == '__main__':
+    mp.set_start_method('spawn')
     app.run(debug=True)
 
